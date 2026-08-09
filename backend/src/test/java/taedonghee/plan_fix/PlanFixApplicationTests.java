bf79afd382961a2a495a4d610d28bc3efbec1b40
Koul <dongik0122@gmail.com>
2026-08-09 09:52:44 +0900

    chore: 백엔드 초기 커밋

---
+++ backend/src/test/java/taedonghee/plan_fix/PlanFixApplicationTests.java
@@ -0,0 +1,13 @@
+package taedonghee.plan_fix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanFixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
