5b44a861852bc5f0c7a409966c442940ab5493ed
Koul <dongik0122@gmail.com>
2026-08-21 02:07:13 +0900

    chore: spot 도메인 재설계를 위해 기존 구현 제거 및 example 코드 제거

--- backend/src/main/java/taedonghee/plan_fix/infrastructure/spot/SpotJpaRepository.java
+++
@@ -1,9 +0,0 @@
-package taedonghee.plan_fix.infrastructure.spot;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-/**
- * spots 테이블 Spring Data JPA Repository
- */
-public interface SpotJpaRepository extends JpaRepository<SpotJpaEntity, Long> {
-}
